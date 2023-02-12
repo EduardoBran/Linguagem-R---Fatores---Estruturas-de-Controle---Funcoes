74958b21856f29b21ff8d873ed80e5cbfb3fa303
Eduardo Brandao <eduardo.ads1814@gmail.com>
2023-02-12 19:34:17 -0300

    funcoes especiais unlist docall strsplit - parte 2

--- 07-Funcoes-Especiais-unlist-docall-e-strsplit.R
+++ 07-Funcoes-Especiais-unlist-docall-e-strsplit.R
@@ -19,3 +19,11 @@ unlist(lista1)
 vec1 <- unlist(lista1)
 class(vec1)
 vec1
+
+lista2 <- list(v1 = 6, v2 = list(381, 2190), v3 = c(30, 217))
+lista2
+
+unlist(lista2)
+
+mean(unlist(lista2))
+round(mean(unlist(lista2)))
